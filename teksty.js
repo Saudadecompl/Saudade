ea47bcf77850b0347a4d2af5382fc6e077f94306
Saudadecompl <jaroslawboczarski@gmail.com>
2026-03-22 21:55:13 +0100

    teksty.js

--- teksty.js
+++ teksty.js
@@ -5,9 +5,23 @@
 */
 
 const WIERSZE = [
-    "1\nIdziesz wolniej.\nWidać namysł\nW twoich oczach.\n\nCiało już tu jest.\nMyśli biegną za nim.",
-    "2.\nSkrócone:\nMyśli.\nOddech.\n\nChcemy zmusić\nchwilę—\n\nby pomieściła\nwięcej.",
-    "3.\nWidzę:\nmrużysz oczy.\n\nTo cię zdradza.\n\nNie musisz mówić.\n\nCzujesz\ntę chwilę."
+    "\nIdziesz wolniej.\nWidać namysł\nW twoich oczach.\n\nCiało już tu jest.\nMyśli biegną za nim.",
+    "\nSkrócone:\nMyśli.\nOddech.\n\nChcemy zmusić\nchwilę—\n\nby pomieściła\nwięcej.",
+    "\nWidzę:\nmrużysz oczy.\n\nTo cię zdradza.\n\nNie musisz mówić.\n\nCzujesz\ntę chwilę."
+    "Wciąż o nich myślę.\n\nO ścieżkach,\nktórymi\nnie pójdę.\n\nUstach,\nktórych nie pocałuję.\n\nRamionach,\nktórych nie obejmę.\n\nSłowach,\nktórych nie wypowiem.",
+    "Powietrze gęstnieje,\nmówisz.\n\nTak.",
+    "Czyjaś ręka.\nCzyjeś westchnienie.\nCzyjeś „chodź”.\n\nMelodia—\nbez nut.",
+    "Wiersze to głównie\npauzy.\n\nMam nadzieję,\nże słowa wybaczą\nzdradę ich tajemnic.",
+    "Nie wytrzymuję\nciśnienia słów.\n\nPodobno\njestem gadatliwy.\n\nGdybym trzymał je\nw środku—\n\neksplodowałbym.",
+    "Przyznaję się.\n\nWinny.\n\nSłowa mnie\nzdradzają.\n\nWiedzą\nlepiej ode mnie.",
+    "Znowu masz\nza długie rękawy.\nLedwo widać\npaznokcie.\n\nWiesz,\njak to działa?",
+    "Nie zginaj rąk.\nNie — nie rób tego.\n\nPoddaję się\njuż teraz.",
+    "Pierwsze chwile\nsą tylko raz.\n\nTo okrutne.",
+    "Czekam,\naż się odwrócisz.\n\nWiesz.\n\nOboje\nto wiemy —\nbez słów.\n\nPowietrze gęstnieje.\n\nIskry.",
+    "Miały być\nlotnogórne.\n\nWyszły:\ndurne,\nwtórne.\n\nCzoło\nchmurne.",
+    "Przyzwyczajenie.\n\nSłowo i uczucie,\nktóre powinno być\nzakazane.",
+    "Najpierw —\npożądanie.\n\nPotem —\npo żądanie.",
+    "Grawitacja,\njako forma żeńska,\nma\ndamskich\nprzedstawicieli."
 ];
 
 const GLOSY = [
